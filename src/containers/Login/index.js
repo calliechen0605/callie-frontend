d47ef18524368592a02a27c91c382beb338044b6
Callie Chen <calliechen@Callies-Air.attlocal.net>
2022-08-03 11:44:54 -0400

    feat: 🎸 added log demo by antd-mobile

---
+++ src/containers/Login/index.js
@@ -0,0 +1,53 @@
+import {
+  Button, Input, Form, Dialog,
+} from 'antd-mobile';
+import { loginService } from '../../services/login';
+import './index.css';
+
+const initialValues = {
+  username: '哈哈哈',
+  password: '12345',
+};
+
+const Login = () => {
+  const [form] = Form.useForm();
+
+  const onSubmit = async () => {
+    const values = form.getFieldsValue();
+    const res = await loginService(values.username, values.password);
+    if (res && res.length > 0) {
+      Dialog.alert({
+        content: '登录成功',
+      });
+      return;
+    }
+    Dialog.alert({
+      content: '登录失败',
+    });
+  };
+
+  return (
+    <div className="login">
+      <Form
+        form={form}
+        layout="horizontal"
+        mode="card"
+        initialValues={initialValues}
+        footer={(
+          <Button block color="primary" onClick={onSubmit} size="large">
+            登录
+          </Button>
+          )}
+      >
+        <Form.Item label="用户名" name="username">
+          <Input placeholder="请输入用户名" clearable />
+        </Form.Item>
+        <Form.Item label="密码" name="password">
+          <Input placeholder="请输入密码" clearable type="password" />
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+
+export default Login;
